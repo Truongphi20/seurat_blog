b442fa24acaa8b68a02a71d8cd6975aeb2abb215
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-05-13 15:27:15 +0000

    update: add test data

---
+++ tutorials/pbmc3k_tutorial.R
@@ -0,0 +1,10 @@
+## Setup the Seurat Object
+
+library(dplyr)
+library(Seurat)
+library(patchwork)
+
+# Load the PBMC dataset
+pbmc.data <- Read10X(data.dir = "test_data/pbmc3k_filtered_gene_bc_matrices/filtered_gene_bc_matrices/hg19")
+# Initialize the Seurat object with the raw (non-normalized data).
+pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
