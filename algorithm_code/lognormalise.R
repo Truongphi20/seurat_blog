00084c3b525e788a81d73828e7b52f34eb0018fd
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-05-17 04:17:01 +0000

    update: digging normalise

--- algorithm_code/lognormalise.R
+++ algorithm_code/lognormalise.R
@@ -2,6 +2,29 @@ library(dplyr)
 library(Seurat)
 library(patchwork)
 
+# Load debugable binary
+dyn.unload("/usr/local/lib/R/site-library/SeuratObject/libs/SeuratObject.so")
+dyn.load("/workspaces/seurat_blog/commands/seurat-5.5.0/src/build/SeuratObject.so")
+
+# commands/seurat-5.5.0/R/RcppExports.R:20
+LogNorm <- function(data, scale_factor, display_progress = TRUE) {
+    .Call('_Seurat_LogNorm', PACKAGE = 'Seurat', data, scale_factor, display_progress)
+}
+
+# commands/seurat-5.5.0/R/preprocessing.R:4865
+LogNormalize.V3Matrix <- function(
+  data,
+  scale.factor = 1e4,
+  margin = 2L,
+  verbose = TRUE,
+  ...
+){
+    norm.data <- LogNorm(data, scale_factor = scale.factor, display_progress = verbose)
+    colnames(x = norm.data) <- colnames(x = data)
+    rownames(x = norm.data) <- rownames(x = data)
+    return(norm.data)
+}
+
 # commands/seurat-5.5.0/R/preprocessing.R:4912
 NormalizeData.V3Matrix <- function(
   object,
@@ -12,7 +35,7 @@ NormalizeData.V3Matrix <- function(
   verbose = TRUE,
   ...
 ){
-    normalized.data <- LogNormalize(
+    normalized.data <- LogNormalize.V3Matrix(
         data = object,
         scale.factor = scale.factor,
         verbose = verbose
@@ -78,6 +101,7 @@ pbmc.data <- Read10X(data.dir = "test_data/pbmc3k_filtered_gene_bc_matrices/filt
 # Initialize the Seurat object with the raw (non-normalized data).
 pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
 
+# Normalise
 pbmc <- NormalizeData.Seurat(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
 
 
