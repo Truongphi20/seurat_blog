b10fea3360b1710b82d52e7eb13076e84b4e0676
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-05-27 05:54:44 +0000

    update: clean up scaling

--- algorithm_code/ScaleData.R
+++ algorithm_code/ScaleData.R
@@ -4,7 +4,11 @@ library(patchwork)
 library(Matrix)
 
 # commands/seurat-5.5.0/src/data_manipulation.cpp:148
-FastSparseRowScale <- function(mat, scale = TRUE, center = TRUE, scale_max = 10, display_progress = TRUE) {
+FastSparseRowScale <- function(mat, scale = TRUE, center = TRUE, scale_max = 10) {
+
+  scale = TRUE
+  center = TRUE
+  scale_max = 10
    
   # Get dimensions of original matrix
   n_rows <- nrow(mat)
@@ -73,26 +77,13 @@ FastSparseRowScale <- function(mat, scale = TRUE, center = TRUE, scale_max = 10,
 }
 
 # commands/seurat-5.5.0/R/preprocessing.R:5111
-ScaleData.default <- function(
-  object,
-  features = NULL,
-  vars.to.regress = NULL,
-  latent.data = NULL,
-  split.by = NULL,
-  model.use = 'linear',
-  use.umi = FALSE,
-  do.scale = TRUE,
-  do.center = TRUE,
-  scale.max = 10,
-  block.size = 1000,
-  min.cells.to.block = 3000,
-  verbose = TRUE,
-  ...
-){
+ScaleData.default <- function(object, features = NULL)
+{
+    block.size = 1000
+
     features <- as.vector(x = intersect(x = features, y = rownames(x = object)))
     object <- object[features, , drop = FALSE]
     object.names <- dimnames(x = object)
-    min.cells.to.block <- min(min.cells.to.block, ncol(x = object))
 
     split.by <- TRUE
     split.cells <- split(x = colnames(x = object), f = split.by)
@@ -110,18 +101,11 @@ ScaleData.default <- function(
     for (i in 1:max.block) {
         my.inds <- ((block.size * (i - 1)):(block.size * i - 1)) + 1
         my.inds <- my.inds[my.inds <= length(x = features)]
-        arg.list <- list(
-            mat = object[features[my.inds], split.cells[["TRUE"]], drop = FALSE],
-            scale = do.scale,
-            center = do.center,
-            scale_max = scale.max,
-            display_progress = FALSE
-        )
+        arg.list <- list(mat = object[features[my.inds], split.cells[["TRUE"]], drop = FALSE])
         arg.list <- arg.list[intersect(x = names(x = arg.list), y = names(x = formals(fun = scale.function)))]
         data.scale <- do.call(what = scale.function, args = arg.list)
         dimnames(x = data.scale) <- dimnames(x = object[features[my.inds], split.cells[["TRUE"]]])
         scaled.data[features[my.inds], split.cells[["TRUE"]]] <- data.scale
-        rm(data.scale)
     }
 
     dimnames(x = scaled.data) <- object.names
@@ -131,48 +115,18 @@ ScaleData.default <- function(
 }
 
 # commands/seurat-5.5.0/R/preprocessing5.R:359
-ScaleData.StdAssay <- function(
-  object,
-  features = NULL,
-  layer = 'data',
-  vars.to.regress = NULL,
-  latent.data = NULL,
-  by.layer = FALSE,
-  split.by = NULL,
-  model.use = 'linear',
-  use.umi = FALSE,
-  do.scale= TRUE,
-  do.center = TRUE,
-  scale.max = 10,
-  block.size = 1000,
-  min.cells.to.block = 3000,
-  save = 'scale.data',
-  verbose = TRUE,
-  ...
-){
-    use.umi <- FALSE
+ScaleData.StdAssay <- function(object, features = NULL)
+{
     layer <- 'data'
 
     ldata <- LayerData(object = object, layer = layer, features = features)
 
     ldata <- ScaleData.default(
       object = ldata,
-      features = features,
-      vars.to.regress = vars.to.regress,
-      latent.data = latent.data,
-      split.by = split.by,
-      model.use = model.use,
-      use.umi = use.umi,
-      do.scale = do.scale,
-      do.center = do.center,
-      scale.max = scale.max,
-      block.size = block.size,
-      min.cells.to.block = min.cells.to.block,
-      verbose = verbose,
-      ...
+      features = features
     )
 
-    LayerData(object = object, layer = save, features = rownames(ldata)) <- ldata
+    LayerData(object = object, layer = 'scale.data', features = rownames(ldata)) <- ldata
 
     return(object)
 }
@@ -180,39 +134,13 @@ ScaleData.StdAssay <- function(
 # commands/seurat-5.5.0/R/preprocessing.R:5520
 ScaleData.Seurat <- function(
   object,
-  features = NULL,
-  assay = NULL,
-  vars.to.regress = NULL,
-  split.by = NULL,
-  model.use = 'linear',
-  use.umi = FALSE,
-  do.scale = TRUE,
-  do.center = TRUE,
-  scale.max = 10,
-  block.size = 1000,
-  min.cells.to.block = 3000,
-  verbose = TRUE,
-  ...
+  features = NULL
 ) {
     assay <- "RNA"
-    latent.data <- NULL
 
     assay.data <- ScaleData.StdAssay(
-        # object = assay.data,
         object = object[[assay]],
-        features = features,
-        vars.to.regress = vars.to.regress,
-        latent.data = latent.data,
-        split.by = split.by,
-        model.use = model.use,
-        use.umi = use.umi,
-        do.scale = do.scale,
-        do.center = do.center,
-        scale.max = scale.max,
-        block.size = block.size,
-        min.cells.to.block = min.cells.to.block,
-        verbose = verbose,
-        ...
+        features = features
     )
     object[[assay]] <- assay.data
     return(object)
