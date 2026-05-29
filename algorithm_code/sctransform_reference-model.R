04e875a8710c8a34663e23248d8850bfd2ebc70f
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-05-29 09:48:17 +0000

    update: dive in ref

--- algorithm_code/sctransform_reference-model.R
+++ algorithm_code/sctransform_reference-model.R
@@ -4,6 +4,88 @@ library(ggplot2)
 library(sctransform)
 library(Azimuth)
 
+# commands/seurat-5.5.0/R/preprocessing5.R:1115 
+SCTransform.StdAssay <- function(
+  object,
+  layer = 'counts',
+  cell.attr = NULL,
+  reference.SCT.model = NULL,
+  do.correct.umi = TRUE,
+  ncells = 5000,
+  residual.features = NULL,
+  variable.features.n = 3000,
+  variable.features.rv.th = 1.3,
+  vars.to.regress = NULL,
+  latent.data = NULL,
+  do.scale = FALSE,
+  do.center = TRUE,
+  clip.range = c(-sqrt(x = ncol(x = object) / 30), sqrt(x = ncol(x = object) / 30)),
+  vst.flavor = 'v2',
+  conserve.memory = FALSE,
+  return.only.var.genes = TRUE,
+  seed.use = 1448145,
+  verbose = TRUE,
+  ...
+){
+    # Extract TK and TK.
+    layer_name = "counts"
+    layer_counts <- LayerData(object, layer = layer_name)
+    layer_object <- CreateAssayObject(layer_counts)
+
+
+    # Apply SCTransform to each assay in `input_list`.
+    .cell.attr <- cell.attr[Cells(layer_object), ]
+    assay_out <- SCTransform(
+        layer_object,
+        cell.attr = .cell.attr,
+        reference.SCT.model = reference.SCT.model,
+        do.correct.umi = do.correct.umi,
+        ncells = ncells,
+        residual.features = residual.features,
+        variable.features.n = variable.features.n,
+        variable.features.rv.th = variable.features.rv.th,
+        vars.to.regress = vars.to.regress,
+        latent.data = latent.data,
+        do.scale = do.scale,
+        do.center = do.center,
+        clip.range = clip.range,
+        vst.flavor = vst.flavor,
+        conserve.memory = conserve.memory,
+        return.only.var.genes = return.only.var.genes,
+        seed.use = seed.use,
+        verbose = verbose,
+        ...
+    )
+
+    var_features_union <- VariableFeatures(assay_out)
+    all_features_intersect <- rownames(assay_out)
+
+    # Keep features that are variable in at least one output assay/layer but
+    # present in all of them.
+    scale_data_features <- intersect(all_features_intersect, var_features_union)
+
+    # Extract residuals for the selected features and store them in
+    # the outputs scaled.data slot.
+    residuals <- FetchResiduals(
+        object = assay_out, 
+        umi.object = object,
+        features = scale_data_features,
+        verbose = FALSE
+    )
+    
+    LayerData(assay_out, layer = "scale.data") <- residuals
+
+    # Set the output's variable features.
+    VariableFeatures(assay_out) <- VariableFeatures(
+        assay_out, 
+        use.var.features = FALSE,
+        nfeatures = variable.features.n
+    )
+
+    return(assay_out)
+}
+
+# commands/seurat-5.5.0/R/preprocessing.R:4235
 SCTransform.Seurat <- function(
     object,
     assay = "RNA",
@@ -31,7 +113,7 @@ SCTransform.Seurat <- function(
 
     cell.attr <- slot(object = object, name = 'meta.data')[colnames(object[[assay]]),]
 
-    assay.data <- SCTransform(object = object[[assay]],
+    assay.data <- SCTransform.StdAssay(object = object[[assay]],
                         cell.attr = cell.attr,
                         reference.SCT.model = reference.SCT.model,
                         do.correct.umi = do.correct.umi,
