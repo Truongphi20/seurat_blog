ecc9d12ec67f690aded2d75c1252eba04a7b97c2
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-05-29 05:50:28 +0000

    update: dry-run transform

--- algorithm_code/sctransform_reference-model.R
+++ algorithm_code/sctransform_reference-model.R
@@ -1,4 +1,5 @@
 library(Seurat)
+library(SeuratObject)
 library(ggplot2)
 library(sctransform)
 library(Azimuth)
@@ -11,8 +12,49 @@ pbmc <- CreateSeuratObject(counts = pbmc_data)
 pbmc <- PercentageFeatureSet(pbmc, pattern = "^MT-", col.name = "percent.mt")
 
 # Download the reference
-reference <- LoadReference(path = "https://seurat.nygenome.org/azimuth/references/v1.0.0/human_pbmc", seconds = 30L)
+reference <- LoadReference(path = "/workspaces/seurat_blog/test_data/references", seconds = 30L)
 
 
 # run sctransform
-pbmc <- SCTransform(pbmc, vars.to.regress = "percent.mt")
+pbmc <- SCTransform(
+    object = pbmc,
+    assay = "RNA",
+    new.assay.name = "refAssay",
+    residual.features = rownames(x = reference$map),
+    reference.SCT.model = reference$map[["refAssay"]]@SCTModel.list$refmodel,
+    method = 'glmGamPoi',
+    ncells = 2000,
+    n_genes = 2000,
+    do.correct.umi = FALSE,
+    do.scale = FALSE,
+    do.center = TRUE
+)
+
+
+## Checking the result 
+# Extract the full matrix
+mat <- pbmc@assays$refAssay@counts
+
+# Find rows (genes) and columns (cells) with the highest expressions
+top_genes <- order(rowSums(mat > 0), decreasing = TRUE)[1:5]
+top_cells <- order(colSums(mat > 0), decreasing = TRUE)[1:5]
+
+# Subset the matrix using these top indices
+dense_chunk <- mat[top_genes, top_cells]
+
+# Print it as a standard, readable matrix
+print(as.matrix(dense_chunk))
+
+# Expected output 
+#        CCAGTCTGCGGAGA-1 TTACTCGAACGTTG-1 AGAGGTCTACAGCT-1 GCGAAGGAGAGCTT-1
+# TMSB4X              384              189              167              195
+# MALAT1               79               37               56               38
+# B2M                  99              143              131              117
+# RPL13A              167              105               91               56
+# RPL10               116              140               78               54
+#        GGCACGTGTGAGAA-1
+# TMSB4X              216
+# MALAT1               52
+# B2M                  79
+# RPL13A              101
+# RPL10               120
