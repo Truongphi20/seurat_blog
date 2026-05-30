2816f445d193dd5db994d2c84a4074dbb248f0c7
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-05-30 04:09:55 +0000

    update: dive in converve mem

--- algorithm_code/sctransform_conserve-memory.R
+++ algorithm_code/sctransform_conserve-memory.R
@@ -4,6 +4,67 @@ library(ggplot2)
 library(sctransform)
 
 
+# commands/seurat-5.5.0/R/preprocessing.R:4235
+SCTransform.Seurat <- function(
+    object,
+    assay = "RNA",
+    new.assay.name = 'SCT',
+    reference.SCT.model = NULL,
+    do.correct.umi = TRUE,
+    ncells = 5000,
+    residual.features = NULL,
+    variable.features.n = 3000,
+    variable.features.rv.th = 1.3,
+    vars.to.regress = NULL,
+    do.scale = FALSE,
+    do.center = TRUE,
+    clip.range = c(-sqrt(x = ncol(x = object[[assay]]) / 30), sqrt(x = ncol(x = object[[assay]]) / 30)),
+    vst.flavor = "v2",
+    conserve.memory = FALSE,
+    return.only.var.genes = TRUE,
+    seed.use = 1448145,
+    verbose = TRUE,
+    ...
+){
+    set.seed(seed = seed.use)
+    latent.data <- NULL
+    assay <- "RNA"
+
+    cell.attr <- slot(object = object, name = 'meta.data')[colnames(object[[assay]]),]
+
+    assay.data <- SCTransform(object = object[[assay]],
+                        cell.attr = cell.attr,
+                        reference.SCT.model = reference.SCT.model,
+                        do.correct.umi = do.correct.umi,
+                        ncells = ncells,
+                        residual.features = residual.features,
+                        variable.features.n = variable.features.n,
+                        variable.features.rv.th = variable.features.rv.th,
+                        vars.to.regress = vars.to.regress,
+                        latent.data = latent.data,
+                        do.scale = do.scale,
+                        do.center = do.center,
+                        clip.range = clip.range,
+                        vst.flavor = vst.flavor,
+                        conserve.memory = conserve.memory,
+                        return.only.var.genes = return.only.var.genes,
+                        seed.use = seed.use,
+                        verbose = verbose,
+                        ...)
+
+  # Extract all SCT models stored in assay
+  sct_models <- slot(object = assay.data, name = "SCTModel.list")
+  
+  # Update umi.assay field for every SCT model 
+  slot(object = assay.data, name = "SCTModel.list") <- lapply(sct_models, function(model) {
+    slot(model, name = "umi.assay") <- assay
+    model
+  })
+
+  object[[new.assay.name]] <- assay.data
+  return(object)
+}
+
 pbmc_data <- Read10X(data.dir = "test_data/pbmc3k_filtered_gene_bc_matrices/filtered_gene_bc_matrices/hg19")
 pbmc <- CreateSeuratObject(counts = pbmc_data)
 
@@ -11,7 +72,7 @@ pbmc <- CreateSeuratObject(counts = pbmc_data)
 pbmc <- PercentageFeatureSet(pbmc, pattern = "^MT-", col.name = "percent.mt")
 
 # run sctransform
-pbmc <- SCTransform(
+pbmc <- SCTransform.Seurat(
   object = pbmc,
   method = 'glmGamPoi', 
   clip.range = c(-10000, 10),
