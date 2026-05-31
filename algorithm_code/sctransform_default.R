22f120b6527b714ddb68efddba64f27b62038638
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-05-31 08:17:56 +0000

    update: dive in default

--- algorithm_code/sctransform_default.R
+++ algorithm_code/sctransform_default.R
@@ -149,6 +149,21 @@ row_gmean <- function(x, eps = 1) {
     return(ret)
 }
 
+# commands/sctransform-0.4.3/R/utils.R:94
+row_var <- function(x) {
+  if (inherits(x = x, what = 'matrix')) {
+    ret <- matrixStats:::rowVars(x)
+    names(ret) <- rownames(x)
+    return(ret)
+  }
+  if (inherits(x = x, what = 'dgCMatrix')) {
+    ret <- sctransform:::row_var_dgcmatrix(x = x@x, i = x@i, rows = nrow(x), cols = ncol(x))
+    names(ret) <- rownames(x)
+    return(ret)
+  }
+  stop('matrix x needs to be of class matrix or dgCMatrix')
+}
+
 # commands/sctransform-0.4.3/R/vst.R:109
 vst <- function(umi,
                 cell_attr = NULL,
@@ -206,7 +221,7 @@ vst <- function(umi,
     genes_var <- NULL
     # Exclude known poisson genes from the learning step
     genes_amean <- rowMeans(umi)
-    genes_var <- sctransform:::row_var(umi)
+    genes_var <- row_var(umi)
     overdispersion_factor <- genes_var - genes_amean
     overdispersion_factor_step1 <- overdispersion_factor[genes_step1]
     is_overdispersed <- (overdispersion_factor_step1 > 0)
@@ -286,12 +301,12 @@ vst <- function(umi,
       detection_rate = genes_cell_count[genes] / ncol(umi),
       gmean = 10 ^ genes_log_gmean,
       amean = rowMeans(umi),
-      variance = sctransform:::row_var(umi)
+      variance = row_var(umi)
     )
     
     if (ncol(rv$y) > 0) {
       gene_attr$residual_mean = rowMeans(rv$y)
-      gene_attr$residual_variance = sctransform:::row_var(rv$y)
+      gene_attr$residual_variance = row_var(rv$y)
     }
 
     rv[['gene_attr']] <- gene_attr
