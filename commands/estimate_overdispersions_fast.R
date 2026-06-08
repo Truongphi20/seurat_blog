ea6d7c84544ae365e25b5379a8b70ec625cec69c
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-06-08 14:00:39 +0000

    update: set up command for est overdispersion

---
+++ commands/estimate_overdispersions_fast.R
@@ -0,0 +1,24 @@
+library(glmGamPoi)
+library(beachmat)
+
+## Load unoptimized shared lib
+so_path <- "/usr/local/lib/R/site-library/glmGamPoi/libs/glmGamPoi.so"
+if (is.loaded(so_path)) {
+    dyn.unload(so_path)
+}
+dyn.load("/workspaces/seurat_blog/commands/glmGamPoi_1.24.0/src/build/glmGamPoi.so")
+
+## Read stack variables
+mystack <- readRDS("/workspaces/seurat_blog/test_data/inputs_estimate_overdispersions_fast.rds")
+
+## Run estimation of overdispersions
+est <- estimate_overdispersions_fast(
+    initializeCpp(mystack$y), 
+    initializeCpp(mystack$mean), 
+    mystack$model_matrix, 
+    mystack$do_cox_reid_adjustment, 
+    mystack$n_subsamples, 
+    mystack$max_iter
+)
+
+print(est$estimate)
