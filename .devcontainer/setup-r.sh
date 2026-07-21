0eb5c3d7143a620c92da7c6912373b35d3d19ac2
Truongphi20 <phinguyenphinguyen2000@gmail.com>
2026-07-21 09:39:32 +0000

    update: build up R as devcontainer post command

---
+++ .devcontainer/setup-r.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+R_SRC_DIR="${CONTAINER_WORKSPACE_FOLDER:-/workspaces/seurat_blog}/commands/r-source-4.5.2"
+
+if [ -d "$R_SRC_DIR" ]; then
+    echo "=== Setting up R source build in $R_SRC_DIR ==="
+    cd "$R_SRC_DIR"
+
+    # 1. Configure R with debug flags and shlib support
+    ./configure \
+        --enable-R-shlib \
+        --without-recommended-packages \
+        CFLAGS="-g -O0" \
+        FFLAGS="-g -O0"
+
+    # 2. Set up dummy SVN / documentation files to bypass svnonly checks
+    mkdir -p .svn doc
+    touch doc/FAQ
+    echo "Revision: 86000" > SVN-REVISION
+    echo "Revision: 86000" > SVN-REVISION-tmp
+    rm -f non-tarball
+
+    # 3. Build R
+    make GIT="echo Revision: 86000" SVN="echo"
+else
+    echo "Directory $R_SRC_DIR not found. Skipping R build."
+fi
